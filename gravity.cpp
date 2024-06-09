2b11705e456515ba0bc1d9a03395aaed2110ae43
Rafael Alfaro <rafalfaro_18@outlook.com>
2024-06-09 17:28:59 -0600

    Adds a new grid for gravity

--- gravity.cpp
+++ gravity.cpp
@@ -11,8 +11,19 @@ int main()
             /*voxel size=*/0.5, /*width=*/4.0);
     // Associate some metadata with the grid.
     grid->insertMeta("radius", openvdb::FloatMetadata(50.0));
-    // Name the grid "LevelSetSphere".
-    grid->setName("LevelSetSphere");
+    // Name the grid "density".
+    grid->setName("density");
+
+    // Add the grid pointer to a container.
+    openvdb::GridPtrVec grids;
+    grids.push_back(grid);
+
+    // Create an empty velocity grid with gravity as background value
+    auto gravity = openvdb::Vec3SGrid::create(openvdb::Vec3s(0, -9.81, 0));
+    gravity->setName("velocity");
+    gravity->insertMeta("class", openvdb::StringMetadata("staggered"));
+    grids.push_back(gravity);
+    
     // Create a VDB file object and write out the grid.
-    openvdb::io::File("gravity.vdb").write({grid});
+    openvdb::io::File("gravity.vdb").write(grids);
 }
