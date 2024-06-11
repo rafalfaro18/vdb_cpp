8213ef22562c5f94dea26b40efd31d9f87a46f8b
Rafael Alfaro <rafalfaro_18@outlook.com>
2024-06-10 22:52:13 -0600

    Successfully advect points grid by gravity velocity grid

--- gravity.cpp
+++ gravity.cpp
@@ -1,6 +1,9 @@
 #include <openvdb/openvdb.h>
 #include <openvdb/tools/LevelSetSphere.h>
 #include <openvdb/tools/VolumeAdvect.h>
+#include <openvdb/points/PointAdvect.h>
+#include <iostream>
+#include <openvdb/points/PointCount.h>
 
 std::string my_to_string2(int n, int width = 4)
 {
@@ -11,93 +14,85 @@ std::string my_to_string2(int n, int width = 4)
 
 int main()
 {
+    // Initialize grid types and point attributes types.
     openvdb::initialize();
-    // Create an empty floating-point grid with background value 0.
-    openvdb::FloatGrid::Ptr grid = openvdb::FloatGrid::create();
-
-    // Get an accessor for coordinate-based access to voxels.
-    openvdb::FloatGrid::Accessor accessor = grid->getAccessor();
-    // Define a coordinate with large signed indices.
-    openvdb::Coord xyz(1, -2, 3);
-    accessor.setValue(xyz, 0);
-
-    xyz.reset(1, 2, -3);
-    accessor.setValue(xyz, 0.0);
-
-    xyz.reset(1, -100, 1);
-    accessor.setValue(xyz, 1.0);
-
-    xyz.reset(1, 100, 1);
-    accessor.setValue(xyz, 1.0);
-
-    xyz.reset(1, 0, 100);
-    accessor.setValue(xyz, 1.0);
-
-    xyz.reset(1, 0, -100);
-    accessor.setValue(xyz, 1.0);
-
-    xyz.reset(100, 0, 0);
-    accessor.setValue(xyz, 1.0);
-
-    xyz.reset(-100, 0, 0);
-    accessor.setValue(xyz, 1.0);
-
-    // Associate a scaling transform with the grid that sets the voxel size
-    // to 1 units in world space.
-    grid->setTransform(openvdb::math::Transform::createLinearTransform(/*voxel size=*/1));
-    
-    // Name the grid "density".
-    grid->setName("density");
-    
-    // Identify the grid as a fog volume.
-    grid->setGridClass(openvdb::GRID_FOG_VOLUME);
-
-    // Add the grid pointer to a container.
-    openvdb::GridPtrVec grids;
-    grids.push_back(grid);
+    // Generate a level set grid.
+    openvdb::FloatGrid::Ptr sphereGrid =
+        openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(/*radius=*/20.0,
+            /*center=*/openvdb::Vec3f(1.5, 2, 3), /*voxel size=*/0.5);
+    // Retrieve the number of leaf nodes in the grid.
+    openvdb::Index leafCount = sphereGrid->tree().leafCount();
+    // Use the topology to create a PointDataTree
+    openvdb::points::PointDataTree::Ptr pointTree(
+        new openvdb::points::PointDataTree(sphereGrid->tree(), 0, openvdb::TopologyCopy()));
+    // Ensure all tiles have been voxelized
+    pointTree->voxelizeActiveTiles();
+    // Define the position type and codec using fixed-point 16-bit compression.
+    using PositionAttribute = openvdb::points::TypedAttributeArray<openvdb::Vec3f,
+        openvdb::points::FixedPointCodec<false>>;
+    openvdb::NamePair positionType = PositionAttribute::attributeType();
+    // Create a new Attribute Descriptor with position only
+    openvdb::points::AttributeSet::Descriptor::Ptr descriptor(
+        openvdb::points::AttributeSet::Descriptor::create(positionType));
+    // Determine the number of points / voxel and points / leaf.
+    openvdb::Index pointsPerVoxel = 8;
+    openvdb::Index voxelsPerLeaf = openvdb::points::PointDataGrid::TreeType::LeafNodeType::SIZE;
+    openvdb::Index pointsPerLeaf = pointsPerVoxel * voxelsPerLeaf;
+    // Iterate over the leaf nodes in the point tree.
+    for (auto leafIter = pointTree->beginLeaf(); leafIter; ++leafIter) {
+        // Initialize the attributes using the descriptor and point count.
+        leafIter->initializeAttributes(descriptor, pointsPerLeaf);
+        // Initialize the voxel offsets
+        openvdb::Index offset(0);
+        for (openvdb::Index index = 0; index < voxelsPerLeaf; ++index) {
+            offset += pointsPerVoxel;
+            leafIter->setOffsetOn(index, offset);
+        }
+    }
+    // Create the points grid.
+    openvdb::points::PointDataGrid::Ptr points =
+        openvdb::points::PointDataGrid::create(pointTree);
+    // Set the name of the grid.
+    points->setName("Points");
+    // Copy the transform from the sphere grid.
+    points->setTransform(sphereGrid->transform().copy());
+    // Randomize the point positions.
+    std::mt19937 generator(/*seed=*/0);
+    std::uniform_real_distribution<> distribution(-0.5, 0.5);
+    // Iterate over the leaf nodes in the point tree.
+    for (auto leafIter = points->tree().beginLeaf(); leafIter; ++leafIter) {
+        // Create an AttributeWriteHandle for position.
+        // Note that the handle only requires the value type, not the codec.
+        openvdb::points::AttributeArray& array = leafIter->attributeArray("P");
+        openvdb::points::AttributeWriteHandle<openvdb::Vec3f> handle(array);
+        // Iterate over the point indices in the leaf.
+        for (auto indexIter = leafIter->beginIndexOn(); indexIter; ++indexIter) {
+            // Compute a new random position (in the range -0.5 => 0.5).
+            openvdb::Vec3f positionVoxelSpace(distribution(generator));
+            // Set the position of this point.
+            // As point positions are stored relative to the voxel center, it is
+            // not necessary to convert these voxel space values into
+            // world-space during this process.
+            handle.set(*indexIter, positionVoxelSpace);
+        }
+    }
 
     // Create an empty velocity grid with gravity as background value
-    auto velocity = openvdb::Vec3SGrid::create();
-    openvdb::Vec3SGrid::Accessor accessorV = velocity->getAccessor();
-    xyz.reset(1, -2, 3);
-    accessorV.setValue(xyz, openvdb::Vec3s(0, 0.5, 0));
-
-    xyz.reset(1, 2, -3);
-    accessorV.setValue(xyz, openvdb::Vec3s(0, 0.5, 0));
-
-    // Associate a scaling transform with the grid that sets the voxel size
-    // to 1 units in world space.
-    velocity->setTransform(openvdb::math::Transform::createLinearTransform(/*voxel size=*/1));
+    auto gravity = openvdb::Vec3SGrid::create(openvdb::Vec3s(0, -9.81, 0));
     
-    velocity->setName("velocity");
-    // Identify the grid as a staggered.
-    velocity->setGridClass(openvdb::GRID_STAGGERED);
-    grids.push_back(velocity);
-
     // Create a VDB file object and write out the grid.
-    openvdb::io::File("velocity_0000.vdb").write(grids);
+    openvdb::io::File("velocity_0000.vdb").write({gravity, points});
     for (int i = 1; i<=250; i++)
     {
-        auto advect = openvdb::tools::VolumeAdvection(*velocity);
-        // Add the grid pointer to a container.
-        openvdb::GridPtrVec newGrids;
 
-        const float slowdown = 0.1;
-        float newDensity = i * slowdown;
-
-        //auto newGrid = advect.advect<openvdb::FloatGrid, openvdb::tools::PointSampler>(*grid, i);
-        xyz.reset(1, -2, 3);
-        accessor.setValue(xyz, accessor.getValue(xyz) + newDensity);
-
-        xyz.reset(1, 2, -3);
-        accessor.setValue(xyz, accessor.getValue(xyz) + newDensity);
-        
-        newGrids.push_back(grid);
+        // Advect points in-place using gravity velocity grid
+        openvdb::points::advectPoints(*points, *gravity,
+            /*integrationOrder=*/4, /*dt=*/1.0/24.0, /*timeSteps=*/1);
         
         std::string name = "velocity_" + my_to_string2(i) + ".vdb";
         
-        newGrids.push_back(velocity);
-        openvdb::io::File(name).write(newGrids);    
+        
+        openvdb::io::File(name).write({gravity, points});    
     }
     
 }
